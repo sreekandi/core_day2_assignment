b6de3a785475449301d8c6249c86c4c2430b709e
Kandi <sree-vidhya.kandi@capgemini.com>
2019-03-16 11:17:47 +0530

    added assignments

---
+++ bin/tests/TriangleTest.java
@@ -0,0 +1,71 @@
+package tests;
+import org.junit.Test;
+import main.Triangle;
+import static org.junit.Assert.*;
+
+public class TriangleTest
+{
+	
+	
+
+	
+	@Test
+	public boolean rightAngletriangle()
+	{
+		Triangle triangle=new Triangle(3,4,5);
+
+		assertTrue(triangle.isRight());
+	}
+	@Test
+	public boolean angleTriangle()
+	{
+		Triangle triangle=new Triangle(2,4,5);
+
+		assertFalse(triangle.isRight());
+	}
+	@Test
+	public void testTriangleIsScalene(){
+	
+		Triangle triangle= new Triangle(1,4,2);
+		assertTrue(triangle.isScalene());
+	
+	}
+	@Test
+	public void testTriangleIsNotIsScalene(){
+	
+		Triangle triangle= new Triangle(1,1,2);
+		assertFalse(triangle.isScalene());
+	
+	}
+	
+	@Test
+	public void testTriangleIsNotIsosceles(){
+	
+		Triangle triangle= new Triangle(1,4,2);
+		assertFalse(triangle.isIsosceles());
+	
+	}
+	@Test
+	public void testTriangleIsIsosceles(){
+	
+		Triangle triangle= new Triangle(1,1,2);
+		assertTrue(triangle.isIsosceles());
+	
+	}
+
+	@Test
+	public void testTriangleIsNotEquilateral(){
+	
+		Triangle triangle= new Triangle(1,4,2);
+		assertFalse(triangle.isEquilateral());
+	
+	}
+	@Test
+	public void testTriangleIsEquilateral(){
+	
+		Triangle triangle= new Triangle(1,1,1);
+		assertTrue(triangle.isEquilateral());
+	
+	}
+	
+} 
